a2fdb0c08423a1068e439501aa6fc1579e5625b3
alessandroraffa <alessandroraffa@192.168.1.124>
2012-11-09 21:03:43 +0100

    added Angus Croll credit :-)

--- bootstrapModal.js
+++ bootstrapModal.js
@@ -66,8 +66,7 @@
       }
 
       /**
-       * @see     ...
-       * @author  ...
+       * @see     http://javascriptweblog.wordpress.com/2011/08/08/fixing-the-javascript-typeof-operator/
        */
       var ___checkType = function(obj) {
         return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
